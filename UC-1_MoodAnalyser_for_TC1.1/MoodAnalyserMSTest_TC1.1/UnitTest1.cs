fac832e3a9095d9d1d729bc839b61518250e497e
Sagarxox <109059306+Sagarxox@users.noreply.github.com>
2022-12-26 22:30:51 +0530

    Mood Analyser For TC-1.1

---
+++ UC-1_MoodAnalyser_for_TC1.1/MoodAnalyserMSTest_TC1.1/UnitTest1.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UC_2_MoodAnalyseAppWithCore;
+
+namespace MoodAnalyserMSTestWithCore
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenSadMoodShouldReturnSAD()
+        {
+            // Arrange
+            string expected = "SAD";
+            string message = "I am in Sad Mood";
+            MoodAnalyser moodAnalyse = new MoodAnalyser(message);
+
+            //Act
+            string mood = moodAnalyse.AnalyseMood();
+
+            //Assert
+            Assert.AreEqual(expected, mood);
+        }
+    }
+}
